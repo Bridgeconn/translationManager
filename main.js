7b6a0d896f4aeba713449193a61ad9bc85fef83f
Ayush Nigam <ayushnigamdps@yahoo.co.in>
2016-11-17 18:01:35 +0530

    Console logs Removed

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ app.on('window-all-closed', () => {
 app.on('ready', function() {
   // Create the browser window.
   mainWindow = new BrowserWindow({icon:'images/TC_Icon_logo.png',width: 800, height: 600});
-  mainWindow.toggleDevTools();
+  //mainWindow.toggleDevTools();
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
