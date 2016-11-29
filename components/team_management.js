9e17ed344012ead64688ec2d831b2fa7b71ee2f9
Ayush Nigam <ayushnigamdps@yahoo.co.in>
2016-11-29 21:51:46 +0530

    Creating JSON for new team

--- components/team_management.js
+++ components/team_management.js
@@ -9,35 +9,48 @@ const FormControl = require('react-bootstrap/lib/FormControl');
 const Button = require('react-bootstrap/lib/Button');
 const style = require("./Style");
 const Table = require('./table');
-const file = ('./static/teamname.json');
-const nameData = require('../static/name.json');
-const ReactSelectize = require("react-selectize");
-const MultiSelect = ReactSelectize.MultiSelect;
+const file = ('./static/team.json');
+const teamnameData = require('../static/teamname.json');
+const teamData = require('../static/team.json');
+const _ = require('lodash');
+const ReactBsTable = require("react-bootstrap-table");
+//const Select = require('react-select');
+const ReactSuperSelect = require('react-super-select');
 
 class TeamManagement extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { component: <div><Table /></div> , makes:nameData } 
-    }
-
-    handleChange(name, e) {
-      var change = {};
+        this.state = {};
+        console.log(teamData);
+}
+    handleInputChange(name, e) {
+      let change = {};
       change[name] = e.target.value;
       this.setState(change);
     }
 
-    handleSubmit() {       
-        var obj =  {table:[]};                   
-        var obj1 = this.state.input1;
-        var obj2 = this.state.make;
-        console.log(obj2);
-        obj.table.push({id: obj1});
+    handlerExample(options) {
+      let output = [];
+      _.map(options, function(option){
+        output = output.concat([option.name]);
+        global.multiselect = output
+        console.log(multiselect);
+      })
+
+    };
+   
+    handleSubmit() {  
+        let obj =  {table:[]};                   
+        let obj1 = this.state.input1;
+        let obj2 = multiselect;
+        //console.log(obj2);
+        obj.table.push({id: obj1 , membername: obj2});
         console.log(obj)
         fs.readFile(file, (err, data) => {
             if (err) throw err;
-            var filedata = JSON.parse(data);
-            console.log(obj.table);
-            filedata.table.push(obj.table);
+            let filedata = JSON.parse(data);
+            console.log(obj);
+            filedata.push(obj);
             fs.writeFile(file, JSON.stringify(filedata), function(err){
             if (err) throw err;
             console.log(filedata);
@@ -48,30 +61,37 @@ class TeamManagement extends React.Component {
     }
 
     render() {
-        var component = this.state.component;
-         var names = this;
-      
+        var element = [];
+       for  (var i = 0, len = teamData.length; i < len; ++i) {
+              element.push(teamData[i].table);
+              //console.log(JSON.stringifyteamData[i]);              
+              }
+              console.log(element);
+
         return (<div>
             <div className="container fluid" style={{ marginLeft: '90px' }}>
+
                 <Form>
-                <FormGroup controlId="formInlineName">
-                    <div><MultiSelect options = { this.state.makes.map(function(make) {
-                        return { label: make.label, value: make.id };
-                    })
-                    } placeholder = "Select Names" value = { this.state.make } 
-                            onValueChange = { function(make) {
-                            names.setState ({make: make, model: undefined}
-                            )}}>                  
-                    </MultiSelect></div>
-                </FormGroup>
+                <ReactSuperSelect placeholder="Make Your Selections" 
+                  dataSource={teamnameData} 
+                  onChange={this.handlerExample} 
+                  multiple={true}
+                  tags={true}
+                  keepOpenOnSelection={true}
+                   />
+
                 <FormGroup controlId="formInlineName">
                   <ControlLabel>Team Name</ControlLabel>
                   {' '}
-                  <FormControl type="text" placeholder="Enter the Name" value={this.state.input1} onChange={this.handleChange.bind(this, 'input1')}/>
+                  <FormControl type="text" placeholder="Enter the Name" value={this.state.input1} onChange={this.handleInputChange.bind(this, 'input1')}/>
                 </FormGroup>
-                    {' '}                                    
+                    {' '} 
+                <BootstrapTable data={element}>
+                    <TableHeaderColumn dataField="id" isKey={true}>Team Name</TableHeaderColumn>
+                    <TableHeaderColumn dataField="membername">Names</TableHeaderColumn>    
+                </BootstrapTable>
+                                   
                 </Form></div>
-                {component}
                 <Button type="submit" style={{ marginLeft: '90px' }} onClick={() => this.handleSubmit()}>Submit</Button>
             </div>
         );
