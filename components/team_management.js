473b1f4d058443cdd64808ef0ac3a958f4d27454
Ayush Nigam <ayushnigamdps@yahoo.co.in>
2016-11-30 18:43:48 +0530

    Creating JSON issue Resolved

--- components/team_management.js
+++ components/team_management.js
@@ -1,6 +1,5 @@
 const React = require('react');
 const bootstrap = require('react-bootstrap');
-//const jsonfile = require('jsonfile');
 const fs = require('fs');
 const FormGroup = require('react-bootstrap/lib/FormGroup');
 const Form = require('react-bootstrap/lib/Form');
@@ -9,90 +8,90 @@ const FormControl = require('react-bootstrap/lib/FormControl');
 const Button = require('react-bootstrap/lib/Button');
 const style = require("./Style");
 const Table = require('./table');
+const _ = require('lodash');
+const ReactBsTable = require("react-bootstrap-table");
+const ReactSuperSelect = require('react-super-select');
 const file = ('./static/team.json');
 const teamnameData = require('../static/teamname.json');
 const teamData = require('../static/team.json');
-const _ = require('lodash');
-const ReactBsTable = require("react-bootstrap-table");
-//const Select = require('react-select');
-const ReactSuperSelect = require('react-super-select');
 
 class TeamManagement extends React.Component {
     constructor(props) {
         super(props);
         this.state = {};
-        console.log(teamData);
 }
+
+
     handleInputChange(name, e) {
       let change = {};
       change[name] = e.target.value;
       this.setState(change);
     }
 
-    handlerExample(options) {
+    handlerDropdown(options) {
       let output = [];
       _.map(options, function(option){
         output = output.concat([option.name]);
         global.multiselect = output
-        console.log(multiselect);
       })
-
     };
-   
-    handleSubmit() {  
-        let obj =  {table:[]};                   
+
+    handleSubmit(e) {
+        let obj =  [{table:{}}];                   
         let obj1 = this.state.input1;
-        let obj2 = multiselect;
-        //console.log(obj2);
-        obj.table.push({id: obj1 , membername: obj2});
-        console.log(obj)
-        fs.readFile(file, (err, data) => {
-            if (err) throw err;
-            let filedata = JSON.parse(data);
-            console.log(obj);
-            filedata.push(obj);
-            fs.writeFile(file, JSON.stringify(filedata), function(err){
-            if (err) throw err;
-            console.log(filedata);
-            console.log('The "data to append" was appended to file!');
-            });    
-        });
- 
-    }
+        if ( typeof multiselect === 'undefined'){
+            alert("Please select the name");
+        }else{
+            let obj2 = multiselect;
+            obj =({id: obj1 , membername: obj2});
+            var result = this.refs.table.handleAddRow(obj);
+            if(result){  
+              alert(result);
+            }
+            else{
+                fs.readFile(file, (err, data) => {
+                    if (err) throw err;
+                    let filedata = JSON.parse(data);
+                    filedata.push(obj);
+                    fs.writeFile(file, JSON.stringify(filedata), function(err){
+                        if (err) throw err;
+                        console.log('The "data to append" was appended to file!');
+                    }); 
+                    //setTimeout(function() {this.setState({input1 : ''});}.bind(this), 3000);  
+                });
+            }
+            
+        }    
+    };
 
     render() {
-        var element = [];
-       for  (var i = 0, len = teamData.length; i < len; ++i) {
-              element.push(teamData[i].table);
-              //console.log(JSON.stringifyteamData[i]);              
-              }
-              console.log(element);
-
-        return (<div>
+        return  (
             <div className="container fluid" style={{ marginLeft: '90px' }}>
+                <div >
+                    <Form>
+                        <ReactSuperSelect placeholder="Select the name" 
+                          dataSource={teamnameData} 
+                          onChange={this.handlerDropdown} 
+                          multiple={true}
+                          tags={true}
+                          keepOpenOnSelection={true}
+                          clearSearchOnSelection = {true}
+                          />
 
-                <Form>
-                <ReactSuperSelect placeholder="Make Your Selections" 
-                  dataSource={teamnameData} 
-                  onChange={this.handlerExample} 
-                  multiple={true}
-                  tags={true}
-                  keepOpenOnSelection={true}
-                   />
+                        <FormGroup controlId="formInlineName">
+                        <ControlLabel>Team Name</ControlLabel>
+                        {' '}
+                        <FormControl type="text" placeholder="Enter the Name" ref="table" value={this.state.input1} onChange={this.handleInputChange.bind(this, 'input1')}/>
+                        </FormGroup>
+                            {' '} 
+                    </Form>
+                </div>
 
-                <FormGroup controlId="formInlineName">
-                  <ControlLabel>Team Name</ControlLabel>
-                  {' '}
-                  <FormControl type="text" placeholder="Enter the Name" value={this.state.input1} onChange={this.handleInputChange.bind(this, 'input1')}/>
-                </FormGroup>
-                    {' '} 
-                <BootstrapTable data={element}>
+                <Button type="submit" style={{ position: 'left' }} onClick={() => this.handleSubmit()}>Add Team</Button>
+                <BootstrapTable ref="table" data={teamData} >
                     <TableHeaderColumn dataField="id" isKey={true}>Team Name</TableHeaderColumn>
                     <TableHeaderColumn dataField="membername">Names</TableHeaderColumn>    
-                </BootstrapTable>
-                                   
-                </Form></div>
-                <Button type="submit" style={{ marginLeft: '90px' }} onClick={() => this.handleSubmit()}>Submit</Button>
+                </BootstrapTable>                 
             </div>
         );
     }
