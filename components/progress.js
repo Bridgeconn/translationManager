76e00babe51e4521c7797ae43220fd993180ca57
Ayush Nigam <ayushnigamdps@yahoo.co.in>
2017-01-31 17:52:43 +0530

    Prototype v0.1

--- components/progress.js
+++ components/progress.js
@@ -34,19 +34,21 @@ class Progressbar extends React.Component {
 		for (var n = 0 ; n < result.length ; n++) {
 			var output = [];
 			var doneAssignments = _.filter(result[n].details,  { 'isCompleted': 'Done' });
-
 			for (var i = 0 ; i < doneAssignments.length ; i++){
 				var temp = {};
 				if (typeof doneAssignments[i].Milestones !== 'undefined') {
 					temp['BookName'] = doneAssignments[i].Book;
 					temp['Milestone'] = doneAssignments[i].Milestones;
 					temp['Project'] = doneAssignments[i].Project;
+					temp['Count'] = 1;
 					var book = doneAssignments[i].Book;
 					var bookChapters = chaptersdata[book];
 					temp['progress'] = 1/bookChapters*100;
 					var z = (_.findIndex(output, { 'BookName': doneAssignments[i].Book , 'Milestone': doneAssignments[i].Milestones }));
 					if( z > -1 ){
 						output[z].progress = output[z].progress + 1/bookChapters*100;
+						output[z].Count = output[z].Count + 1;
+
 					}else{
 						output.push(temp);
 					}	
@@ -67,21 +69,26 @@ class Progressbar extends React.Component {
 				);				
 			}
 		);
-		
+
 		for (var i = 0; i < arrProgress.length; i++) {
 			console.log(arrProgress[i].Milestone);
 			var x = arrProgress.filter(function(value){ 
 			return value.Milestone === arrProgress[i].Milestone && value.Project === arrProgress[i].Project }).length;
 			console.log(x);
-			arrProgress[i].totalProgress = x/1189*100;
+			if (arrProgress[i].Count > 1) {
+				arrProgress[i].totalProgress = arrProgress[i].Count/1189*100;
+			}else{
+				arrProgress[i].totalProgress = x/1189*100;
+			}
 		}
 
 		console.log(arrProgress);
 		fs.writeFileSync(resultfile, JSON.stringify(arrProgress),'utf8');               
 		var resultdata = JSON.parse(fs.readFileSync(resultfile, 'utf8'));
 		var projectData = _.uniq(resultdata);
-		//console.log(projectData);
-		this.state = { label: resultdata, projectLabel: projectData };
+		var xyz =  _.uniqBy(projectData,'Milestone');
+		console.log(xyz);
+		this.state = { label: resultdata, projectLabel: xyz };
 	}
 
 	render() {
@@ -113,7 +120,8 @@ class Progressbar extends React.Component {
 				})     
 					return (
 			<div className="container fluid" style={{ marginLeft: '90px' }}>
-				<div><Grid><Row className="show-grid"><Col sm={2} md={6}>{progressComponents}</Col><Col sm={2} md={6}>{projectComponent}</Col></Row></Grid></div>
+				<Grid><Row className="show-grid"><h4><strong>Book Progress</strong></h4>
+				<Col sm={2} md={6}>{progressComponents}</Col><h4><strong>Overall Progress</strong></h4><Col sm={2} md={6}>{projectComponent}</Col></Row></Grid>	
 		    </div>
 		)	
 	}       
