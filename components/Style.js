347e245da778fb235ad95d2cd96c5f15dbc118b7
Christopher Klapp <christopher@klapp.name>
2017-03-05 18:31:20 -0500

    Merge more Progress UI updates

--- components/Style.js
+++ components/Style.js
@@ -79,12 +79,18 @@ var style = {
     textAlign:"center",
     width:"20%",
     paddingTop:"10px",
-    paddingBottom:"10px",
+    paddingBottom:"25px",
   },
 
    milestoneName:{
     margin:"0 auto",
     textAlign:"center",
+  },
+
+  progressTableHeader:{
+    margin:"0 auto",
+    textAlign:"center",
+    backgroundColor: "#eee"
   },
 
   circleGreen:{
@@ -146,7 +152,8 @@ var style = {
     marginBottom:"0px"
   },
   purpleProgressBar:{
-    backgroundColor:"#A900FF"
+    backgroundColor:"#A900FF",
+    marginBottom:"0px"
   }
 };
 
