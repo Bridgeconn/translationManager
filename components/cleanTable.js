da862c746dd1e6254cf7ae4f33a47029754784d2
Christopher Klapp <christopher@klapp.name>
2017-03-05 23:45:20 -0500

    Delete and Edits now work again. TODO: Fix Check Boxes

--- components/cleanTable.js
+++ components/cleanTable.js
@@ -12,77 +12,73 @@ class CleanTable extends React.Component {
 	constructor(props) {
     super(props)
     this.state = {}
-	}
 
-	writeFile(filename, filedata) {
 		let that = this
-		fs.writeFile(that.props.filename, JSON.stringify(filedata), function(err){
-			if (err) throw err
-			that.props.callback()
-		})
-	}
-
-	onDeleteRow(rows) {
-		let that = this
-		fs.readFile(that.props.filename, (err, data) => {
-			var filedata = JSON.parse(data)
-			for (var n=0; n < filedata.length; n++) {
-				if (filedata[n].name == rows) {
-					var removedObject = filedata.splice(n,1)
-					removedObject = null
-					break
+		this.actions = {
+			writeFile: function(filename, filedata, callback) {
+				fs.writeFile(filename, JSON.stringify(filedata), function(err){
+					if (err) throw err
+					callback()
+				})
+			},
+			onDeleteRow: function(rows) {
+				fs.readFile(props.filename, (err, data) => {
+					var filedata = JSON.parse(data)
+					for (var n=0; n < filedata.length; n++) {
+						if (filedata[n].name == rows) {
+							var removedObject = filedata.splice(n,1)
+							removedObject = null
+							break
+						}
+					}
+					if (err) throw err
+					that.actions.writeFile(props.filename, filedata, props.callback)
+				})
+			},
+			afterSaveCell: function(row, cellName, cellValue) {
+				fs.readFile(props.filename, (err, data) => {
+					var filedata = JSON.parse(data)
+					for (var n=0; n < filedata.length; n++) {
+						if (filedata[n].name == row.name) {
+							filedata[n][cellName] = cellValue
+							break
+						}
+					}
+					if (err) throw err
+					that.actions.writeFile(props.filename, filedata, props.callback)
+				})
+			},
+			//Character Validation Check - Checks if length < 5 and cant be left blank.
+			characterValidator: function(value) {
+				const response = { isValid: true, notification: { type: 'success', msg: '', title: '' } };
+				if (!value) {
+					response.isValid = false;
+					response.notification.type = 'error';
+					response.notification.msg = 'Value must be inserted';
+					response.notification.title = 'Requested Value';
+				} else if (value.length < 5) {
+					response.isValid = false;
+					response.notification.type = 'error';
+					response.notification.msg = 'Value must have 5+ characters';
+					response.notification.title = 'Invalid Value';
 				}
-			}
-			if (err) throw err
-			writeFile(that.props.filename, filedata)
-		})
-	}
-
-	afterSaveCell(row, cellName, cellValue) {
-		let that = this
-		fs.readFile(this.props.filename, (err, data) => {
-			var filedata = JSON.parse(data)
-			for (var n=0; n < filedata.length; n++) {
-				if (filedata[n].name == row.name) {
-					filedata[n][cellName] = cellValue
-					break
+				return response;
+			},
+			//Integer Validation Check - Checks if Number is entered and cant be left blank.
+			integerValidator: function(value) {
+				const nan = isNaN(parseInt(value, 10));
+				if (nan) {
+					return 'Must be a integer!';
 				}
+				else if (!value) {
+					response.isValid = false;
+					response.notification.type = 'error';
+					response.notification.msg = 'Value must be inserted';
+					response.notification.title = 'Requested Value';
+				}
+				return true;
 			}
-			if (err) throw err
-			writeFile(that.props.filename, filedata)
-		})
-	}
-
-	//Character Validation Check - Checks if length < 5 and cant be left blank.
-	characterValidator(value) {
-		const response = { isValid: true, notification: { type: 'success', msg: '', title: '' } };
-		if (!value) {
-			response.isValid = false;
-			response.notification.type = 'error';
-			response.notification.msg = 'Value must be inserted';
-			response.notification.title = 'Requested Value';
-		} else if (value.length < 5) {
-			response.isValid = false;
-			response.notification.type = 'error';
-			response.notification.msg = 'Value must have 5+ characters';
-			response.notification.title = 'Invalid Value';
 		}
-		return response;
-	}
-
-	//Integer Validation Check - Checks if Number is entered and cant be left blank.
-	integerValidator(value) {
-		const nan = isNaN(parseInt(value, 10));
-		if (nan) {
-			return 'Must be a integer!';
-		}
-		else if (!value) {
-			response.isValid = false;
-			response.notification.type = 'error';
-			response.notification.msg = 'Value must be inserted';
-			response.notification.title = 'Requested Value';
-		}
-		return true;
 	}
 
 	render() {
@@ -90,14 +86,14 @@ class CleanTable extends React.Component {
 		const cellEdit = {
 			mode: 'dbclick',
 			blurToSave: true,
-			afterSaveCell: that.afterSaveCell
+			afterSaveCell: this.actions.afterSaveCell
 		}
 		const selectRow = {
 			mode: 'radio',
 			clickToSelect: true
 		}
 		const options = {
-			onDeleteRow: that.onDeleteRow
+			onDeleteRow: this.actions.onDeleteRow
 		}
 		const tooltip = (
 			<Tooltip id="tooltip">Double click the cell to edit</Tooltip>
@@ -105,7 +101,7 @@ class CleanTable extends React.Component {
 		function activeFormatter(cell, row, enumObject, index) {
 			console.log(row.isCompleted);
 			return (
-				<input type='checkbox' title="Click to edit" key={index} onChange={that.afterSaveCell.bind(this,row)} checked={row.isCompleted} active={ cell } />
+				<input type='checkbox' title="Click to edit" key={index} onChange={that.actions.afterSaveCell.bind(this,row)} checked={row.isCompleted} active={ cell } />
 			)
 		}
 
@@ -113,10 +109,13 @@ class CleanTable extends React.Component {
 			let editable = {}
 			switch(field.validation) {
 				case 'text':
-				editable = { validator: that.characterValidator }
+				editable = { validator: that.actions.characterValidator }
 				break
 				case 'integer':
-				editable = { validator: that.integerValidator }
+				editable = { validator: that.actions.integerValidator }
+				break
+				case 'boolean':
+				editable = false
 				break
 				default:
 				editable = { }
